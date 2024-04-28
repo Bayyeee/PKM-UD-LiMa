7bdf6abf57b9ea23a800c0c83fdc32d8ef7f54af
caynine9 <tantowijauhari16@gmail.com>
2024-04-28 12:58:05 +0800

    Mengisi fragment home, menggabung fragment ke package fragment

--- app/src/main/java/com/project/pkm_ud_lima/fragment/CatatanFragment.kt
+++ app/src/main/java/com/project/pkm_ud_lima/fragment/CatatanFragment.kt
@@ -1,18 +1,18 @@
-package com.project.pkm_ud_lima
+package com.project.pkm_ud_lima.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.project.pkm_ud_lima.databinding.FragmentHomeBinding
+import com.project.pkm_ud_lima.databinding.FragmentCatatanBinding
 
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class CatatanFragment : Fragment() {
+    private lateinit var binding: FragmentCatatanBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View { binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View { binding = FragmentCatatanBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
